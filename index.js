94b60b4ef175c501513c320e05613f901cc8f423
Ankit Jangir <ankitjangirnice@gmail.com>
2022-08-08 15:59:08 +0530

    added new endpoint, event fetching using event _id from url parmas

--- index.js
+++ index.js
@@ -16,11 +16,20 @@ app.use(fileUpload({
 }));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/frontend/build/index.html');
 })
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+//------------------for hosting on heroku--------------
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+    });
+   }
+   //------------------for hosting-------------
 
 app.listen(PORT, ()=>{
     console.log("listening on port " + PORT);
